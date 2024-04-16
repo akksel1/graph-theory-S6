ff861784d9af2f93e4a7151246a3d23c4e005b57
Perkinss27 <cyprien.gibault@efrei.net>
2024-04-16 19:33:00 +0200

    Added graph structure, added print_graph function and changes on the build function. (Part 1 and 2 done).

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import utilities as utils
 from graph import Graph
 
 def main():
-    raw_data = utils.extract_data("table 8")
-    graph_8 = Graph(data=raw_data, name="Graph 8")
+    raw_data = utils.extract_data("table 10")
+    graph_8 = Graph(data=raw_data, name="Graph 10")
     graph_8.print_constraint_table()
+    graph_8.print_graph()
     return 0
 
 if __name__ == "__main__":
